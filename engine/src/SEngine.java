2ecb8a69df54ff468cb61ff06306e849dd4593c7
shai <micimous123@gmail.com>
2025-08-02 19:37:17 +0300

    add interface SEngine

---
+++ engine/src/SEngine.java
@@ -0,0 +1,19 @@
+public interface SEngine {
+//    // Command 1: Load a program
+//    void loadProgramFromXml(String filePath) throws InvalidProgramException, FileNotFoundException;
+//
+//    // Command 2: Show the program details
+//    ProgramInfoDTO getProgramDetails();
+//
+//    // Command 3: Expand the program
+//    ExpandedProgramDTO expandProgram(int expansionLevel);
+//
+//    // Command 4: Run the program
+//    RunResultDTO runProgram(int expansionLevel, List<Integer> inputs);
+//
+//    // Command 5: Get run history
+//    List<RunHistoryDTO> getRunHistory();
+//
+//    // Helper to know if a program is loaded
+//    boolean isProgramLoaded();
+}

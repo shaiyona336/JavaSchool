4390104b00067c1875f00092ada29f3506505fc4
shai <micimous123@gmail.com>
2025-08-03 20:13:50 +0300

    added program to MachineState and improved program

--- engine/src/parts/MachineState.java
+++ engine/src/parts/MachineState.java
@@ -3,6 +3,7 @@ package parts;
 import java.util.Map;
 
 public class MachineState {
+    private Program program; //potentially change to final
     private Map<String, Integer> variables; //potentially change <string,integar> to a class
     private int programCounter;
     private int totalCycles;
--- engine/src/parts/Program.java
+++ engine/src/parts/Program.java
@@ -3,5 +3,17 @@ package parts;
 import java.util.List;
 
 public class Program {
-    private List<Instruction> variables;
+    private final List<Instruction> instructions; //potentially final
+
+    public Program(List<Instruction> instructions) {
+        this.instructions = instructions;
+    }
+
+    public Instruction get_instruction(int programCounter) {
+        assert instructions != null : "program is not set";
+        assert programCounter >= 0 && programCounter < instructions.size() : "programCounter out of bounds";
+
+        return instructions.get(programCounter);
+    }
+
 }
